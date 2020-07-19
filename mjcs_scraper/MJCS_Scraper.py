bb8749a651b27ed56285341a7278b2398069d196
Peter Durham <peterdurham@Peters-MBP.fios-router.home>
2020-07-19 11:05:38 -0400

    added function for searching all location code. However the locations do not seem to matter when using the search by Case ID function. Searching for a Montgomery County case with the District Court location still returns the Montgomery County case

--- mjcs_scraper/MJCS_Scraper.py
+++ mjcs_scraper/MJCS_Scraper.py
@@ -16,6 +16,35 @@ class MJCS_Scraper:
 		self.disclaimer_accepted = False
 		self.session = requests.session()
 
+		self.location_codes = {
+			"District Court" : "DC",
+			"Allegany County" : "01",
+			"Anne Arrundel County" : "02",
+			"Baltimore City" : "24",
+			"Baltimore County" : "03",
+			"Calvert County" : "04",
+			"Caroline County" : "05",
+			"Carroll County" : "06",
+			"Cecil County" : "07",
+			"Charles County" : "08",
+			"Dorchester County" : "09",
+			"Frederick County" : "10",
+			"Garrett County" : "11",
+			"Harford" : "12",
+			"Howard County" : "13",
+			"Kent County" : "14",
+			"Montgomery County" : "15",
+			"Prince George's County" : "16",
+			"Queen Anne's County" : "17",
+			"Saint Mary's County" : "18",
+			"Somerset County" : "19",
+			"Talbot County" : "20",
+			"Washington County" : "21",
+			"Wicomico County" : "22",
+			"Worcester County" : "23"
+		}
+
+	#Accepts disclaimer and passes cookie to the Requests Session
 	def accept_disclaimer(self):
 		#Request home page
 		self.driver.get(self.url)
@@ -51,15 +80,12 @@ class MJCS_Scraper:
 
 		return results
 
-	def close(self):
-		self.driver.close()
-
-	def seach_case_id_post(self, case_id):
+	def seach_case_id_post(self, case_id, location_code):
 		if not self.disclaimer_accepted:
 			self.accept_disclaimer()
 		#Define POST Request
 		searchRequest = {
-			"locationCode": "DC",
+			"locationCode": location_code,
 			"caseId" : case_id,
 			"action" : "Get Case"
 		}
@@ -71,7 +97,23 @@ class MJCS_Scraper:
 		else:
 			results = False
 		return results
+		
+	def search_all_locations(self, case_id):
+		if not self.disclaimer_accepted:
+			self.accept_disclaimer()
+		
+		#Iterate through locations and search for case_id
+		for key, value in self.location_codes.items():
+			result = self.seach_case_id_post(case_id, value)
 
+			if result:
+				return {"found" : result, "location" : key}
+
+			#Case not found
+			return {"found" : False, "location" : "None"}
+
+	def close(self):
+		self.driver.close()
 #Takes response and returns whether or not the case was found
 def parse_response(response):
 	#Parse Response
